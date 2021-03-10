38dcf00f392e4388ac8c6c4015e35860b70204df
EtiennePestre <etiennepestre@yahoo.fr>
2021-03-10 13:55:14 +0100

    connection email ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCZj_a4Y7-EN7qKsyIrxXZYiLKPLxMtTcc",
+    authDomain: "todo-34ac7.firebaseapp.com",
+    projectId: "todo-34ac7",
+    storageBucket: "todo-34ac7.appspot.com",
+    messagingSenderId: "557186447867",
+    appId: "1:557186447867:web:b62f5ca1928f557638f35e",
+    measurementId: "G-7G8JMQHE9P"
+  }
 };
